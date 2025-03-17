ea51e498d8e575b9942fbe386a1fbbdc4d0f969a
MatthieuMosch <mmosch@gmail.com>
2025-03-17 17:20:33 +0100

    knop om post toe te voegen

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import './App.css'
 import {useState} from "react";
 import axios from"axios";
+import fakePost from "./constants/fakePost.json";
+import newPost from "./constants/newPost.json";
 
 function App() {
     const URI = "http://localhost:3000";
@@ -10,7 +12,7 @@ function App() {
     const [posts, setPosts] = useState([]);
     const [post, setPost] = useState({});
 
-    //as 'normal' named function
+    //a 'normal' function to fetch all posts
     async function fetchAllPosts(){
         try{
             setIsLoading(true);
@@ -25,7 +27,7 @@ function App() {
         }
     }
 
-    //as named arrow function
+    //an arrow function to fetch 1 post
     const fetchPost  = async (id) => {
         try{
             setIsLoading(true);
@@ -38,7 +40,25 @@ function App() {
         } finally {
             setIsLoading(false);
         }
+    }
 
+    //a 'normal' function to add a  posts
+    async function addPost(post){
+        try{
+            setIsLoading(true);
+            const response = await axios.post(`${URI}/posts`, post);
+            console.log(response);
+            if (response.statusText==="Created"){
+                console.log("succesmelding");
+            } else {
+                console.error("een foutmelding");
+            }
+        } catch(err) {
+            setErrorMsg(err.message);
+            console.error(err);
+        } finally {
+            setIsLoading(false);
+        }
     }
 
     return (
@@ -47,18 +67,22 @@ function App() {
             <main>
                 <button type="button" onClick={fetchAllPosts}>haal alle posts op</button>
                 <button type="button" onClick={()=>fetchPost(6)}>haal post 6 op</button>
-                {isLoading && <p>loading...</p>}
-                {errorMsg && <p>{errorMsg}</p>}
-                { posts.length > 0 ?
-                    <ul>
-                        {
-                            posts.map(post => (
-                                <li key={post.id}>{post.title}</li>
-                            ))
-                        }
-                    </ul> : null
-                }
-                {post && <p>{post.content}</p>}
+                <button type="button" onClick={()=>addPost(fakePost)}>voeg een fake post toe</button>
+                <button type="button" onClick={()=>addPost(newPost)}>voeg een nieuwe post toe</button>
+                <article>
+                    {isLoading && <p>loading...</p>}
+                    {errorMsg && <p>{errorMsg}</p>}
+                    { posts.length > 0 ?
+                        <ul>
+                            {
+                                posts.map(post => (
+                                    <li key={post.id}>{post.title}</li>
+                                ))
+                            }
+                        </ul> : null
+                    }
+                    {post && <p>{post.content}</p>}
+                </article>
             </main>
         </>
     )
